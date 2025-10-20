63100e2ac2d980c5bd7763766fc62e3d83dacd16
dsamykov1996 <dsamykov1996@gmail.com>
2025-10-20 14:02:22 +0200

    tasks

---
+++ task.py
@@ -0,0 +1,41 @@
+#Task 1
+x = (1+2+3)
+print (x)
+
+#Task 2
+y = int(10/2)
+print (y)
+
+#Task 3
+s = int(5*5)
+print (s)
+
+#Task 6
+num = int(input("Enter any number:"))
+print(num % 10)
+
+#Task 7
+n1 = int(input("Enter number 1"))
+n2 = int(input("Enter number 2"))
+# a = 0
+a = n1
+n1 = n2
+n2 = a
+print(n1,n2)
+
+#Task 4
+minutes = 5
+seconds = minutes * 60
+print (seconds)
+
+#Task 5
+#pupils 
+num1 = 5
+#apples
+num2 = 10
+#each kid will receive
+each = num2 // num1
+#rest 
+rest = num2 % num1
+#so
+print(f"each will take {each} , and rest ist {rest}" )
---
+++ zoom.py
@@ -0,0 +1,54 @@
+#Task 1
+
+# password = input("Enter password:")
+# #password = "qwert"
+# # != not =  
+
+# password_accept = "qwert"
+
+# if password == password_accept:
+#     print("Password accepted")
+# # elif password != password accept
+# else:
+#     print("Sorry, that is the wrong password")    
+
+# Task 2
+
+# name1 =input("Enter name1: ")
+# name2 =input("Enter name1: ")
+
+# # if name1 <= name2: 
+# #         print(name1, name2)
+# # else:
+# #         print(name2, name1)      
+
+# if name1 <= name2:
+#     name1, name2 = name2, name1
+
+# print(name2, name1)
+
+
+# Task 3
+
+# number =int(input("Enter_number:"))
+
+# if number == 1:
+#     name_of_number = "One"
+# elif number == 2:
+#     name_of_number = "Two"
+# elif number == 3:
+#     name_of_number = "Three"        
+# else:
+#     name_of_number = "Unknown" 
+# print(name_of_number)      
+# 
+# Task 4
+
+
+letter1 = input("Enter letter1:")
+letter2 = input("Enter letter2:")
+
+if letter1.lower() <= letter2.lower():
+    print("First letter:", letter1, "Second letter:", letter2)
+else:
+    print("First letter:", letter2, "Second letter:", letter1)
