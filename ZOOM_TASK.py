7b6c7568033c21bd20fab4bfef406b872eeb82c2
dsamykov1996 <dsamykov1996@gmail.com>
2025-10-23 16:17:39 +0200

    init git

--- ZOOM_TASK.py
+++ ZOOM_TASK.py
@@ -1,6 +1,6 @@
 # Дано рядок, що складається з n цифр (тобто одноцифрових чисел), між якими стоїть n-1 знаків операцій, кожна з яких може бути або +, або -. Обчисліть значення цього виразу. Програма має надрукувати результат обчислення цього виразу.
 
-# ex = input("Enter ex:\n") # 5-3+1
+ex = input("Enter ex:\n") # 5-3+1
 # # len_ex = len(ex) #5
 
 # n = (len(ex) + 1) // 2 #тоже самое что и int но вместо него //
@@ -12,10 +12,41 @@
 # # ex.replace("-", "+")
 
 
-# # var 2
+# # 2 var
+# # sum = int(ex[0])
+# # for i in range (1, len(ex), 2):
+# #     sum += (1 if ex[i] == "+" else -1) * int(ex[i+1])
 
+# # print(sum)
+
+# # 3 var
 # sum = int(ex[0])
-# for i in range (len(ex)):
-    
-#     int(ex[0]) + (1 if ex[1] == "+" else -1) * int(ex[2]) + (1 if ex[3] == "+" else -1) * int(ex[4])
-1
+# for i in range(1, len(ex)):
+#     if ex[i] == "+":
+#         sum += int(ex[i+1])
+#     elif ex[i] == "-":
+#         sum -= int(ex[i+1])
+# print(sum)
+
+
+
+
+# my_list = ["1", "2", 4, 7]
+# # for el in range(len(my_list)):
+# #     my_list[el] = (my_list[el])
+# # print(my_list)    
+
+# new_list = list(map(int, my_list))
+# print(new_list)
+
+#-------------------
+
+# # 4 var
+
+
+ex = ex.replace("-", "+-")
+parts = ex.split("+")
+
+# map()
+sum_ex = sum(map(int, parts))
+print(sum_ex)
